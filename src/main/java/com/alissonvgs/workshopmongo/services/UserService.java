4c9762feeef12a751a5fe3686470d70723ffa01f
alissonvgs <alyssonvictor00@gmail.com>
2020-05-29 13:12:27 -0300

    Implementacao de Exceptions e obtendo um User por Id - User

--- src/main/java/com/alissonvgs/workshopmongo/services/UserService.java
+++ src/main/java/com/alissonvgs/workshopmongo/services/UserService.java
@@ -1,20 +1,28 @@
 package com.alissonvgs.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alissonvgs.workshopmongo.domain.User;
 import com.alissonvgs.workshopmongo.repository.UserRepository;
+import com.alissonvgs.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+	}
+
 }
