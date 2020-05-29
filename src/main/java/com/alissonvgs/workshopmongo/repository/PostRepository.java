86a5128fe78190f0934f2d2ab75de28bcbd48708
alissonvgs <alyssonvictor00@gmail.com>
2020-05-29 16:57:36 -0300

    Entity Post com User aninhado | Projecao dos dados do autor com DTO | Endpoits - Post

---
+++ src/main/java/com/alissonvgs/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.alissonvgs.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alissonvgs.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+
+}
---
+++ src/main/java/com/alissonvgs/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.alissonvgs.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alissonvgs.workshopmongo.domain.Post;
+import com.alissonvgs.workshopmongo.domain.User;
+import com.alissonvgs.workshopmongo.repository.PostRepository;
+import com.alissonvgs.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+	}
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+}
