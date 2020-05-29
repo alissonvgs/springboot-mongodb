ee94cdf76db35371c90213fef60c33943e1e7bf3
alissonvgs <alyssonvictor00@gmail.com>
2020-05-29 15:20:23 -0300

    Put, Delete e Post implementados - User

--- src/main/java/com/alissonvgs/workshopmongo/services/UserService.java
+++ src/main/java/com/alissonvgs/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alissonvgs.workshopmongo.domain.User;
+import com.alissonvgs.workshopmongo.dto.UserDTO;
 import com.alissonvgs.workshopmongo.repository.UserRepository;
 import com.alissonvgs.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,30 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
